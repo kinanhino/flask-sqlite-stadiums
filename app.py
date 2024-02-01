bb677d5b3f1d4e9b0d34024a1d3ea30483c0da0e
kinanhino <kinanhino24@gmail.com>
2024-02-01 21:08:20 +0200

    hiding app secret..

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_migrate import Migrate
 from utilities import db, configure_routes
 import os
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stadiums.db'
